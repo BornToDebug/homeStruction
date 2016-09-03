2d44f484f1385b74630c7ef6a82aa0a468396ad1
Acs David <acs_david97@yahoo.com>
2016-09-03 18:43:24 +0300

    inserting 100 random values in DB and visualizing

--- website/insertInDBtest.py
+++ website/insertInDBtest.py
@@ -6,7 +6,14 @@ django.setup()
 # Importing required models and modules
 from project.models import Temperature
 from django.utils import timezone
+import numpy as np
 
-# Inserting into temperature table with current timestamp
-Temperature.objects.create(value=25, time_recorded=timezone.now())
+randValue = np.random.randint(10, 35, 100)
+randMin = np.random.randint(0, 3600, 100)
+
+# Create 100 temperature objects with random time(last 24 hour) and value
+now = timezone.now()
+for i in range(100):
+    Temperature.objects.create(value=randValue[i], time_recorded=now -
+                               timezone.timedelta(minutes=randMin[i]))
 
--- website/visualizeData.py
+++ website/visualizeData.py
@@ -1,10 +1,36 @@
 import numpy as np
 from matplotlib import pyplot as plt
+from matplotlib import dates
+import django
+import os
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "homeStruction.settings")
+django.setup()
+from project.models import Temperature
+from django.utils import timezone
 
-randArray = np.random.randint(0, 35, 100)
-print randArray
+# Get temperature data from the last 24 hours
+tempArray = Temperature.objects.order_by('-time_recorded').\
+    filter(time_recorded__gte=timezone.now() - timezone.timedelta(days=1))
+
+print tempArray
+
+dateTemp = []
+valueTemp = []
+for item in tempArray:
+    dateTemp.append(item.time_recorded)
+    valueTemp.append(item.value)
+
+print dateTemp, valueTemp
+
+formating = dates.DateFormatter('%H:%M')
+
+print dates.date2num(dateTemp)
 
 cmap = (0, 0, 0)
-plt.plot(randArray, color=cmap), plt.ylabel('temperature C'), plt.xlabel('time')
+
+plt.plot(dateTemp, valueTemp, color=cmap)
+plt.ylabel('temperature C'), plt.xlabel('time')
+plt.xticks(rotation='vertical')
+plt.subplots_adjust(bottom=.3)
 plt.savefig('image.png', bbox_inches='tight', transparent='true')
 plt.show()
