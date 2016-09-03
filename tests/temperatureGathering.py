3708fc471b01b8c57732a1f8e74431e51afb257b
Acs David <acs_david97@yahoo.com>
2016-09-03 20:56:37 +0300

    test file

---
+++ tests/temperatureGathering.py
@@ -0,0 +1,29 @@
+import django
+import os
+from project.models import Temperature
+from django.utils import timezone
+from nanpy import (ArduinoApi, SerialManager)
+
+# Initial setup required
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "homeStruction.settings")
+django.setup()
+
+sensor = 19
+powervoltage = 5
+
+try:
+    connection = SerialManager()
+    a = ArduinoApi(connection=connection)
+except:
+    print("Could not connect to Arduino")
+
+tempavg = 0
+for i in range(0, 10):
+    sensorValue = a.analogRead(sensor)
+    temperature = (sensorValue / 1023) * powervoltage * 100
+    # print("%d: %2.2f" % (i, temperature))
+    tempavg += temperature
+
+# sleep(.2)
+tempavg /= 10.0
+Temperature.objects.create(value=tempavg, time_recorded=timezone.now())
