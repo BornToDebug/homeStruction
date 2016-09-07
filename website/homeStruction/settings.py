7089286584bed483b09fcdfa8f3439eb8d3af872
Acs David <acs_david97@yahoo.com>
2016-09-07 16:42:37 +0300

    template loading fixed

--- website/homeStruction/settings.py
+++ website/homeStruction/settings.py
@@ -57,7 +57,7 @@ ROOT_URLCONF = 'homeStruction.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['templates/'],
+        'DIRS': ['/home/david/git/homeStruction/website/templates/'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
