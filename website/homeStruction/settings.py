9a791b9e49c77d0f08e2d2426d3f9f710b026295
Acs David <acs_david97@yahoo.com>
2016-09-19 13:41:38 +0300

    preparing login page

--- website/homeStruction/settings.py
+++ website/homeStruction/settings.py
@@ -31,11 +31,14 @@ SECRET_KEY = settings_secret.SECRET_KEY
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False  #always
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', '89.120.180.139', '192.168.1.5']
+ALLOWED_HOSTS = [
+        'localhost', '127.0.0.1', '89.120.180.139', '192.168.1.5', 'homestruction.servebeer.com'
+        ]
 
 LOGIN_REDIRECT_URL = ''
 
 LOGIN_URL = '/login/'
+LOGOUT_URL = '/login/'
 
 # Application definition
 
