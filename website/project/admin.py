2b1db85b35ff3d5c6b1fb1655f93608e9c62270c
Acs David <acs_david97@yahoo.com>
2016-09-20 11:02:48 +0300

    database model changes

--- website/project/admin.py
+++ website/project/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
 
-from .models import Temperature
+from .models import Temperature, Door, Lamp, Light
 
 admin.site.register(Temperature)
+admin.site.register(Door)
+admin.site.register(Lamp)
+admin.site.register(Light)
--- website/project/models.py
+++ website/project/models.py
@@ -1,26 +1,39 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.utils import timezone
 
+
+def valueAndTimeToStr(value, time):
+    return str(value) + ' at ' + str(time)
 
 class Temperature(models.Model):
-    time_recorded = models.DateTimeField('Time recorded')
+    time_recorded = models.DateTimeField('Time recorded', default=timezone.now)
     value = models.FloatField()
 
     def __str__(self):
-        return str(self.value)
+        return valueAndTimeToStr(self.value, self.time_recorded) 
 
 
 class Lamp(models.Model):
-    time_switched = models.DateTimeField('Time switched')
-    status = models.CharField(max_length=10)
+    time_recorded = models.DateTimeField('Time recorded', default=timezone.now)
+    value = models.CharField(max_length=10)
 
+    def __str__(self):
+        return valueAndTimeToStr(self.value, self.time_recorded) 
 
 class Light(models.Model):
-    time_recorded = models.DateTimeField('Time recorded')
+    time_recorded = models.DateTimeField('Time recorded', default=timezone.now)
     value = models.FloatField()
-
+    
+    def __str__(self):
+        return valueAndTimeToStr(self.value, self.time_recorded) 
 
 class Door(models.Model):
-    time_recorded = models.DateTimeField('Time opened')
-    status = models.CharField(max_length=10)
+    time_recorded = models.DateTimeField('Time recorded', default=timezone.now)
+    value = models.CharField(max_length=10)
+
+    def __str__(self):
+        return valueAndTimeToStr(self.value, self.time_recorded) 
+
+
