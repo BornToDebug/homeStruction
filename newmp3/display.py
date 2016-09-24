cb9a1beb9e9d7c7d40ede09581e2fd92f277b62d
balintszabo96 <balintsz@yahoo.com>
2016-09-24 17:24:36 +0300

    sdgfsdfg

--- newmp3/display.py
+++ newmp3/display.py
@@ -36,6 +36,7 @@ lcd_rows = 2
 lcd = LCD.Adafruit_CharLCD(lcd_rs, lcd_en, lcd_d4, lcd_d5, lcd_d6, lcd_d7, lcd_columns, lcd_rows, lcd_backlight)
 
 def handler(signal, frame):
+	lcd.clear()
 	lcd.set_backlight(1)
 
 signal.signal(signal.SIGTERM, handler)
