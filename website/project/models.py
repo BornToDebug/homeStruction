07af804e7977a30b42f05be695737bb40ed60b68
balintszabo96 <balintsz@yahoo.com>
2016-09-26 10:59:48 +0300

    dfgdf

--- website/project/models.py
+++ website/project/models.py
@@ -36,4 +36,11 @@ class Door(models.Model):
     def __str__(self):
         return valueAndTimeToStr(self.value, self.time_recorded + timezone.timedelta(hours=3)) 
 
+class Window(models.Model):
+    time_recorded = models.DateTimeField('Time recorded', default=timezone.now)
+    value = models.CharField(max_length=10)
+
+    def __str__(self):
+        return valueAndTimeToStr(self.value, self.time_recorded + timezone.timedelta(hours=3)) 
+
 
