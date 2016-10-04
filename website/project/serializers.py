983819f2799f6c7f3a5bb32067e7db0edf68f144
Acs David <acs_david97@yahoo.com>
2016-10-04 14:46:36 +0300

    ajax

--- website/project/serializers.py
+++ website/project/serializers.py
@@ -11,19 +11,19 @@ class LampSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Lamp
         fields = ('time_recorded', 'value')
- 
+
 class LightSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Light
         fields = ('time_recorded', 'value')
- 
+
 class DoorSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Door
         fields = ('time_recorded', 'value')
- 
+
 class WindowSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Window
         fields = ('time_recorded', 'value')
-                             
+
