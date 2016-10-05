dc8195d8840c4d2be7877b972e2467fd163350cb
nagylilla96 <n.lilla@yahoo.com>
2016-10-05 21:56:12 +0300

    Pinch 'n zoom and double tap images

--- android/HomeStruction/app/src/main/java/com/lilla/homestruction/TemperatureScreen.java
+++ android/HomeStruction/app/src/main/java/com/lilla/homestruction/TemperatureScreen.java
@@ -1,11 +1,12 @@
 package com.lilla.homestruction;
 
-import android.graphics.drawable.Drawable;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.DisplayMetrics;
-import android.widget.ImageView;
-import android.widget.RelativeLayout;
+
+import com.theappguruz.imagezoom.ImageViewTouch;;
+
 
 /**
  * Created by lilla on 22/09/16.
@@ -13,50 +14,31 @@ import android.widget.RelativeLayout;
 
 public class TemperatureScreen extends AppCompatActivity {
 
-//    ImageView imageView;
-//    Matrix matrix = new Matrix();
-//    float scale = 1f;
-//    ScaleGestureDetector SGD;
+    private ImageViewTouch imageViewTouch;
+    private Bitmap myBitmap;
 
 
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.temperature_screen);
+        imageViewTouch = (ImageViewTouch) findViewById(R.id.graph);
 
-//        ImageView imageView = (ImageView) findViewById(R.id.graph);
-//        DisplayMetrics displayMetrics = new DisplayMetrics();
-//        getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
-//        Drawable drawable = getResources().getDrawable(R.drawable.graph);
-//
-//        int hMargin = (int) (displayMetrics.widthPixels * .10);
-//        int vMargin = (int) (displayMetrics.heightPixels * .10);
-//
-//        RelativeLayout.LayoutParams params = new RelativeLayout.LayoutParams(displayMetrics.widthPixels - (hMargin * 2), (int)(displayMetrics.heightPixels) - (vMargin * 2));
-//        params.leftMargin = hMargin;
-//        params.topMargin =  vMargin;
-//        imageView.setLayoutParams(params);
-//        imageView.setImageDrawable(drawable);
+        BitmapFactory.Options options = new BitmapFactory.Options();
+        options.inJustDecodeBounds = true;
 
-        //TODO zoom in on the picture
-//        imageView = (ImageView) findViewById(R.id.graph);
-//        SGD = new ScaleGestureDetector(this, new ScaleListener());
+        myBitmap = BitmapFactory.decodeResource(getResources(), R.drawable.graph, options);
+        if (options.outWidth > 3000 || options.outHeight > 2000) {
+            options.inSampleSize = 4;
+        } else if (options.outWidth > 2000 || options.outHeight > 1500) {
+            options.inSampleSize = 3;
+        } else if (options.outWidth > 1000 || options.outHeight > 1000) {
+            options.inSampleSize = 2;
+        }
+        options.inJustDecodeBounds = false;
+
+        myBitmap = BitmapFactory.decodeResource(getResources(), R.drawable.graph, options);
+        imageViewTouch.setImageBitmapReset(myBitmap, 0, true);
     }
-
-//    private class ScaleListener extends ScaleGestureDetector.SimpleOnScaleGestureListener{
-//
-//        public boolean onScale(ScaleGestureDetector detector){
-//            scale = scale * detector.getScaleFactor();
-//            scale = Math.max(0.1f, Math.min(scale, 5f));
-//            matrix.setScale(scale, scale);
-//            imageView.setImageMatrix(matrix);
-//            return true;
-//        }
-//    }
-//
-//    public boolean onTouchEvent(MotionEvent event){
-//        SGD.onTouchEvent(event);
-//        return true;
-//    }
-
 }
+
