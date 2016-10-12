e794b0794a4ee35ef438ee831cdcaef83a8df3f9
aerika96 <erikatimeaalbert@gmail.com>
2016-10-12 19:07:35 +0300

    add

--- website/project/static/project/rest/rest.js
+++ website/project/static/project/rest/rest.js
@@ -1,4 +1,4 @@
-function getJSON(url, objectConverter, table, button,posVal) {
+function getJSON(url, objectConverter, table,_status, button,posVal) {
 	// try to fetch JSON data from url and call events
 	var json = $.getJSON(url, function(data) {
 		console.log('success');
@@ -17,11 +17,27 @@ function getJSON(url, objectConverter, table, button,posVal) {
 	});
 }
 
+function newJSON(url,_status) {
+	// try to fetch JSON data from url and call events
+	var json = $.getJSON(url, function(data) {
+		console.log('success');
+		_status=data['results'][0].value;
+
+	})
+	.done(function() {
+		console.log('second success')
+	})
+	.fail(function() {
+		//TODO implement error message
+		console.log('error');
+	});
+	
+}
+
 function generateRows(json, table, objectConverter) {
 	//delete current entries in table and populate it with data
 	var tableRow;
 	table.innerHTML = '';
-
 	for (i=0;i<json.length;i++) {
 		tableRow = objectConverter(json[i]);
 		table.innerHTML += tableRow;
