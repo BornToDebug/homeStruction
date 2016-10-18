f7fbdf6a9d2197779eb2803ed8636efd36656d57
Acs David <acs_david97@yahoo.com>
2016-10-18 12:29:45 +0300

    webiste refresh changed

--- website/static/project/alarm.js
+++ website/static/project/alarm.js
@@ -1,19 +1,4 @@
-var timePickerValue = document.getElementById('timepicker').value;
-console.log(timePickerValue);
-var monday = document.getElementById('test1').checked;
-console.log(monday);
-var tuesday = document.getElementById('test2').checked;
-console.log(tuesday);
-var wednesday = document.getElementById('test3').checked;
-console.log(wednesday);
-var thursday = document.getElementById('test4').checked;
-console.log(thursday);
-var friday = document.getElementById('test5').checked;
-console.log(friday);
-var saturday = document.getElementById('test6').checked;
-console.log(saturday);
-var sunday = document.getElementById('test7').checked;
-console.log(sunday);
+
 
 function genURL(timeValue, monday, tuesday, wednesday, thursday, friday, saturday, sunday) {
 	var url = "/setalarm/?";
@@ -24,10 +9,11 @@ function genURL(timeValue, monday, tuesday, wednesday, thursday, friday, saturda
 	var minute = HourMin[1].trim().split(" ")[0];
 	if(HourMin[1].trim().split(" ")[1] === "PM") {
 		hour = (parseInt(hour) + 12).toString();
-		if(hour === "24") {
-			hour = "00";
 		}
-	}
+	
+	
+	
+	
 	console.log("minute:" + minute);
 	url += "hour=" + hour + "&";
 	url += "minute=" + minute + "&";
@@ -55,9 +41,30 @@ function genURL(timeValue, monday, tuesday, wednesday, thursday, friday, saturda
 	return url;
 }
 
-var xhttp = new XMLHttpRequest();
-var url = genURL(timePickerValue, monday, tuesday, wednesday, thursday, friday, saturday, sunday);
-console.log(url);
-xhttp.open("GET", url);
-xhttp.send();
+function setAlarm(){
 
+	var timePickerValue = document.getElementById('timepicker').value;
+	console.log(timePickerValue);
+	var monday = document.getElementById('test1').checked;
+	console.log(monday);
+	var tuesday = document.getElementById('test2').checked;
+	console.log(tuesday);
+	var wednesday = document.getElementById('test3').checked;
+	console.log(wednesday);
+	var thursday = document.getElementById('test4').checked;
+	console.log(thursday);
+	var friday = document.getElementById('test5').checked;
+	console.log(friday);
+	var saturday = document.getElementById('test6').checked;
+	console.log(saturday);
+	var sunday = document.getElementById('test7').checked;
+	console.log(sunday);
+
+
+	var xhttp = new XMLHttpRequest();
+	var url = genURL(timePickerValue, monday, tuesday, wednesday, thursday, friday, saturday, sunday);
+	console.log(url);
+	xhttp.open("GET", url);
+	xhttp.send();
+
+}
