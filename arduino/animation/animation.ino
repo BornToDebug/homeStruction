1c3c856c0c857cabdd97ca8bb0569fccb3383990
nagylilla96 <n.lilla@yahoo.com>
2016-10-29 17:52:52 +0300

    Add blink, and two turn on animations + vid

--- arduino/animation/animation.ino
+++ arduino/animation/animation.ino
@@ -2,6 +2,7 @@
 
 #define RIGHT 6
 #define NUMPIXELS 16
+#define MIDDLE 8
 
 Adafruit_NeoPixel pixels = Adafruit_NeoPixel(NUMPIXELS, RIGHT, NEO_GRB + NEO_KHZ800);
 
@@ -13,14 +14,63 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
-  for (int i = 0; i < NUMPIXELS; i++){
-    pixels.setPixelColor(i, 66, 244, 209);
+  halfCircleWakeUp();
+  reset();
+  fullCircleWakeUp();
+  reset();
+  clip();
+  reset();
+}
+
+void halfCircleWakeUp() {
+  for (int i = 0; i < NUMPIXELS / 4; i++) {
+    pixels.setPixelColor(MIDDLE + i, 0, 255, 0);
+    pixels.setPixelColor(MIDDLE - i - 1, 0, 255, 0);
     pixels.show();
     delay(100);
   }
-  for (int i = 0; i < NUMPIXELS; i++){
+  delay(500);
+}
+
+void fullCircleWakeUp() {
+  for (int i = 0; i < MIDDLE; i++) {
+    pixels.setPixelColor(MIDDLE + i, 0, 255, 0);
+    pixels.setPixelColor(MIDDLE - i - 1, 0, 255, 0);
+    pixels.show();
+    delay(100);
+  }
+  delay(500);
+}
+
+void reset() {
+  for (int i = 0; i < NUMPIXELS; i++) {
     pixels.setPixelColor(i, 0, 0, 0);
     pixels.show();
-    delay(100);
+  }
+  delay(200);
+}
+
+void clip() {
+  for (int j = 0; j < 3; j++) {
+    for (int i = NUMPIXELS / 4 - 1; i >= 0; i--) {
+      pixels.setPixelColor(i, 0, 255, 0);
+      pixels.setPixelColor(NUMPIXELS - i - 1, 0, 255, 0);
+      pixels.setPixelColor(MIDDLE - i - 1, 0, 255, 0);
+      pixels.setPixelColor(MIDDLE + i, 0, 255, 0);
+      pixels.show();
+      delay(50);
+    }
+    delay(200);
+
+    for (int i = 0; i < NUMPIXELS / 4; i++) {
+      pixels.setPixelColor(i, 0, 0, 0);
+      pixels.setPixelColor(NUMPIXELS - i - 1, 0, 0, 0);
+      pixels.setPixelColor(MIDDLE - i - 1, 0, 0, 0);
+      pixels.setPixelColor(MIDDLE + i, 0, 0, 0);
+      pixels.show();
+      delay(50);
+    }
+    delay(200);
   }
 }
+
