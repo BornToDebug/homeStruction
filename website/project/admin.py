354e03c05cb363d8397d0e2d7afeb78a029266f9
Ács Dávid <acs_david97@yahoo.com>
2016-12-10 11:46:57 +0200

    session added to admin

--- website/project/admin.py
+++ website/project/admin.py
@@ -2,6 +2,8 @@ from django.contrib import admin
 
 from .models import Temperature, Door, Lamp, Light, Window, Humidity, Alarm
 
+from django.contrib.sessions.models import Session
+
 admin.site.register(Temperature)
 admin.site.register(Door)
 admin.site.register(Lamp)
@@ -10,3 +12,4 @@ admin.site.register(Window)
 admin.site.register(Humidity)
 admin.site.register(Alarm)
 
+admin.site.register(Session)
